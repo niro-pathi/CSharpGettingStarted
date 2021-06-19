b0bed92952252df619b263be47bffdb3864233c7
Capt. Niro <niroshana.pathirana@crownresorts.com.au>
2021-06-19 12:58:29 +1000

    First commit

---
+++ MyFirstProject/EmptyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyUtilities
+{
+    public class NameUtilities
+    {
+        static public string MyFullName(string fName, string lName)
+        {
+            var fullName = lName + ',' + fName;
+            
+            return "My Full Name is :" + fullName;
+        }
+    }
+}
---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyUtilities;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine(NameUtilities.MyFullName("My First Name", "Last Name"));
+        }
+    }
+}
